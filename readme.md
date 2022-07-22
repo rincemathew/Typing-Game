56815d24fc6ec8e3c3f3e0e5ba5ee242c9a9b980
Rince Mathew <rincemathewmanattu@gmail.com>
2022-07-22 22:06:43 +0530

    js finished

---
+++ readme.md
@@ -0,0 +1,2 @@
+Live link https://rincemathew.github.io/Breakout-Game/
+![alt text](https://github.com/rincemathew/Breakout-Game/blob/master/screenshot.png?raw=true)
--- script.js
+++ script.js
@@ -32,7 +32,6 @@ const words = [
   'loving'
 ];
 
-
 // Init word
 let randomWord;
 
@@ -42,18 +41,65 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Set difficulty select value
+difficultySelect.value =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
 // Update score
 function updateScore() {
-    score++;
-    scoreEl.innerHTML = score;
-  }
+  score++;
+  scoreEl.innerHTML = score;
+}
 
-  addWordToDOM();
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
 
 // Event listeners
 
@@ -68,6 +114,23 @@ text.addEventListener('input', e => {
     // Clear
     e.target.value = '';
 
-    
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
+
+    updateTime();
   }
+});
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 });
